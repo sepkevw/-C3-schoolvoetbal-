e7ad43ca366c71bb23ca14ece74c04f3a18a2a0a
Alaa Aldeen Omaren <d302707@edu.curio.nl>
2026-04-14 09:55:33 +0200

    Update users migration for auth system

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('admin')->default(false);
+            $table->unsignedBigInteger('team_id')->nullable();
             $table->timestamps();
         });
 
